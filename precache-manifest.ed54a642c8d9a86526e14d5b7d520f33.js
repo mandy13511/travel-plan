cc6431608c10e06f15b93837380c1ec2f32f9920
Mandy Chien <lchane2000@gmail.com>
2019-09-18 17:00:33 +0800

    Updates

---
+++ precache-manifest.ed54a642c8d9a86526e14d5b7d520f33.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cea7e9e4875f083c4d5b79742d7c02e",
+    "url": "/travel-plan/index.html"
+  },
+  {
+    "revision": "5462a29dcac0a36b1363",
+    "url": "/travel-plan/static/css/main.651e751f.chunk.css"
+  },
+  {
+    "revision": "082c4cab51c0bd0eb2db",
+    "url": "/travel-plan/static/js/2.4544c903.chunk.js"
+  },
+  {
+    "revision": "5462a29dcac0a36b1363",
+    "url": "/travel-plan/static/js/main.a6a26f29.chunk.js"
+  },
+  {
+    "revision": "a71e242f97acbc7b2f63",
+    "url": "/travel-plan/static/js/runtime~main.315ca04a.js"
+  }
+]);
