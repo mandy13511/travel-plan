9121b72790a5380b60a73f02df6feb08ab05d2ac
Mandy Chien <lchane2000@gmail.com>
2019-10-05 11:09:31 +0800

    Add DragDropList Add react-beautiful-dnd

--- src/EditModal.js
+++ src/EditModal.js
@@ -1,7 +1,109 @@
 import React, { Component } from "react";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 
 import "./styles.scss";
 import "./mbStyles.scss";
+
+// fake data generator
+const getItems = count =>
+  Array.from({ length: count }, (v, k) => k).map(k => ({
+    id: `item-${k}`,
+    content: `item ${k}`
+  }));
+
+// a little function to help us with reordering the result
+const reorder = (list, startIndex, endIndex) => {
+  const result = Array.from(list);
+  const [removed] = result.splice(startIndex, 1);
+  result.splice(endIndex, 0, removed);
+
+  return result;
+};
+
+const grid = 8;
+
+const getItemStyle = (isDragging, draggableStyle) => ({
+  // some basic styles to make the items look a bit nicer
+  userSelect: "none",
+  padding: grid * 2,
+  margin: `0 0 ${grid}px 0`,
+
+  // change background colour if dragging
+  background: isDragging ? "lightgreen" : "grey",
+
+  // styles we need to apply on draggables
+  ...draggableStyle
+});
+
+const getListStyle = isDraggingOver => ({
+  background: isDraggingOver ? "lightblue" : "lightgrey",
+  padding: grid,
+  width: 250
+});
+
+class DragDropTravelList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: getItems(10)
+    };
+    this.onDragEnd = this.onDragEnd.bind(this);
+  }
+
+  onDragEnd(result) {
+    // dropped outside the list
+    if (!result.destination) {
+      return;
+    }
+
+    const items = reorder(
+      this.state.items,
+      result.source.index,
+      result.destination.index
+    );
+
+    this.setState({
+      items
+    });
+  }
+
+  // Normally you would want to split things out into separate components.
+  // But in this example everything is just done in one place for simplicity
+  render() {
+    return (
+      <DragDropContext onDragEnd={this.onDragEnd}>
+        <Droppable droppableId="droppable">
+          {(provided, snapshot) => (
+            <div
+              {...provided.droppableProps}
+              ref={provided.innerRef}
+              style={getListStyle(snapshot.isDraggingOver)}
+            >
+              {this.state.items.map((item, index) => (
+                <Draggable key={item.id} draggableId={item.id} index={index}>
+                  {(provided, snapshot) => (
+                    <div
+                      ref={provided.innerRef}
+                      {...provided.draggableProps}
+                      {...provided.dragHandleProps}
+                      style={getItemStyle(
+                        snapshot.isDragging,
+                        provided.draggableProps.style
+                      )}
+                    >
+                      {item.content}
+                    </div>
+                  )}
+                </Draggable>
+              ))}
+              {provided.placeholder}
+            </div>
+          )}
+        </Droppable>
+      </DragDropContext>
+    );
+  }
+}
 
 export default class EditModal extends Component {
   constructor(props) {
@@ -25,26 +127,6 @@ export default class EditModal extends Component {
         })}
         </div>
 
-      /*
-      <table>
-        <tbody>
-        {this.props.travel.map((element, index) => {
-          return (
-            <tr>
-              <th>Day {index + 1}</th>
-              <td>
-                <ol>
-                  {element.map((item, itemIndex) => {
-                    return <li>{item.place}</li>;
-                  })}
-                </ol>
-              </td>
-            </tr>
-          );
-        })}
-          <tr><th colSpan = "2" >Add New Day</th></tr>
-        </tbody>
-      </table>*/
     );
   }
 
@@ -59,7 +141,7 @@ export default class EditModal extends Component {
           </div>
           <div className="modal-body">
             <div className = "container right"><button>Add below</button><button>Delete</button></div>
-            {this.createTabel()}
+            <DragDropTravelList/>
           </div>
           <div className="modal-footer">
             <span className="text-m">Modal Footer</span>
